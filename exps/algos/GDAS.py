d788986a2a60b99d2b0873478a6874766c208552
Shashank Verma <shashank3959@gmail.com>
2019-11-11 01:39:19 -0600

    Add GDAS search script -- WIP

---
+++ exps/algos/GDAS.py
@@ -0,0 +1,40 @@
+import os, sys, time, glob, random, argparse
+import numpy as np
+from copy import deepcopy
+import torch
+import torch.nn as nn
+# from pathlib import Path
+# lib_dir = (Path(__file__).parent / '..' / '..' / 'lib').resolve()
+# if str(lib_dir) not in sys.path: sys.path.insert(0, str(lib_dir))
+# from config_utils import load_config, dict2config, configure2str
+# from datasets     import get_datasets, SearchDataset
+# from procedures   import prepare_seed, prepare_logger, save_checkpoint, copy_checkpoint, get_optim_scheduler
+# from utils        import get_model_infos, obtain_accuracy
+# from log_utils    import AverageMeter, time_string, convert_secs2time
+# from models       import get_cell_based_tiny_net, get_search_spaces
+
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser("GDAS")
+  parser.add_argument('--data_path',          type=str,   help='Path to dataset')
+  parser.add_argument('--dataset',            type=str,   choices=['cifar10', 'cifar100', 'ImageNet16-120'], help='Choose between Cifar10/100 and ImageNet-16.')
+  # channels and number-of-cells
+  parser.add_argument('--search_space_name',  type=str,   help='The search space name.')
+  parser.add_argument('--max_nodes',          type=int,   help='The maximum number of nodes.')
+  parser.add_argument('--channel',            type=int,   help='The number of channels.')
+  parser.add_argument('--num_cells',          type=int,   help='The number of cells in one stage.')
+  # architecture leraning rate
+  parser.add_argument('--arch_learning_rate', type=float, default=3e-4, help='learning rate for arch encoding')
+  parser.add_argument('--arch_weight_decay',  type=float, default=1e-3, help='weight decay for arch encoding')
+  parser.add_argument('--tau_min',            type=float,               help='The minimum tau for Gumbel')
+  parser.add_argument('--tau_max',            type=float,               help='The maximum tau for Gumbel')
+  # log
+  parser.add_argument('--workers',            type=int,   default=2,    help='number of data loading workers (default: 2)')
+  parser.add_argument('--save_dir',           type=str,   help='Folder to save checkpoints and log.')
+  parser.add_argument('--arch_nas_dataset',   type=str,   help='The path to load the architecture dataset (tiny-nas-benchmark).')
+  parser.add_argument('--print_freq',         type=int,   help='print frequency (default: 200)')
+  parser.add_argument('--rand_seed',          type=int,   help='manual seed')
+  args = parser.parse_args()
+  if args.rand_seed is None or args.rand_seed < 0: args.rand_seed = random.randint(1, 100000)
+  main(args)
