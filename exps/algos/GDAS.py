f1be20c632da559a637846f82f112437022c922e
Shashank Verma <shashank3959@gmail.com>
2019-11-11 13:36:53 -0600

    Add utility to read configs
    Also add support to split the train dataset into
    training and validation sets

--- exps/algos/GDAS.py
+++ exps/algos/GDAS.py
@@ -9,10 +9,9 @@ lib_dir = (Path(__file__).parent / '..' / '..' / 'lib').resolve()
 if str(lib_dir) not in sys.path: sys.path.insert(0, str(lib_dir))
 from procedures   import prepare_seed, prepare_logger
 from log_utils    import AverageMeter, time_string, convert_secs2time
-from datasets     import get_datasets
+from datasets     import get_datasets, SearchDataset
 
-# from config_utils import load_config, dict2config, configure2str
-# from datasets     import get_datasets, SearchDataset
+from config_utils import load_config, dict2config, configure2str
 # from utils        import get_model_infos, obtain_accuracy
 # from models       import get_cell_based_tiny_net, get_search_spaces
 
@@ -36,10 +35,33 @@ def main(xargs):
   # CIFAR Test Transform: [ToTensor(), Normalize(mean, std)]
   train_data, _, xshape, class_num = get_datasets(xargs.dataset, xargs.data_path, -1)
 
+  # Loading the train and validation splits
   if xargs.dataset == 'cifar10' or xargs.dataset == 'cifar100':
     # File path for Train-Validation Split
-    split_Fpath = 'configs/nas-benchmark/cifar-split.txt'
-  print(len(train_data), xshape, class_num)
+    split_Fpath = 'configs/cifar-split.txt'
+    cifar_split = load_config(split_Fpath, None, None)
+    # train: 25000, valid: 25000
+    train_split, valid_split = cifar_split.train, cifar_split.valid
+    logger.log('Load split file from {:}'.format(split_Fpath))
+  elif xargs.dataset.startswith('ImageNet16'):
+    split_Fpath = 'configs/nas-benchmark/{:}-split.txt'.format(xargs.dataset)
+    imagenet16_split = load_config(split_Fpath, None, None)
+    train_split, valid_split = imagenet16_split.train, imagenet16_split.valid
+    logger.log('Load split file from {:}'.format(split_Fpath))
+  else:
+    raise ValueError('invalid dataset : {:}'.format(xargs.dataset))
+
+  # Load GDAS specific scheduler and optimizer configs
+  config_path = 'configs/algos/GDAS.config'
+  config = load_config(config_path, {'class_num': class_num, 'xshape': xshape}, logger)
+
+  # Search Data Loader
+  search_data = SearchDataset(xargs.dataset, train_data, train_split, valid_split)
+  search_loader = torch.utils.data.DataLoader(search_data, batch_size=config.batch_size, shuffle=True,
+                                              num_workers=xargs.workers, pin_memory=True)
+
+
+
 
 
 if __name__ == '__main__':
